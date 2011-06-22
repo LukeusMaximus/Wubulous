ee87aa2f63ca4eda1d91e3028285fc607e8a83ba
Sam Phippen <samphippen@googlemail.com>
2011-06-22 12:22:21 +0100

    fixed up usernamepass.js so it will actually run in a browser
    Signed-off-by: Sam Phippen <samphippen@googlemail.com>

--- usernamepass.js
+++ usernamepass.js
@@ -1,7 +1,5 @@
-md5 = require("./md5");
 
 function hash_username_password(user, pass)
 {
-    return md5.hex_md5(user.toLowerCase()+pass.toLowerCase());
+    return hex_md5(user.toLowerCase()+pass.toLowerCase());
 }
-exports.hashunp = hash_username_password
