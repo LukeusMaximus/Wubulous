e87e1478346ae7eaf60ab9e3997f20857d38ce3c
Sam Phippen <samphippen@googlemail.com>
2011-06-23 15:48:59 +0100

    whitespace fix on url_parsing.js
    Signed-off-by: Sam Phippen <samphippen@googlemail.com>

--- url_parsing.js
+++ url_parsing.js
@@ -2,5 +2,7 @@
  * returns everything after the last slash minus the suffix ".xml"
  */
 function get_job_id_from_url(url) {
-    return url.substr(url.lastIndexOf('/') + 1, url.lastIndexOf('.') - url.lastIndexOf('/') - 1);
+    return url.substr(url.lastIndexOf('/') + 1,
+                      url.lastIndexOf('.') - url.lastIndexOf('/') - 1
+                     );
 }
