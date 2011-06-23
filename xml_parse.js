c3f84588a311c33d110d2ad6cd91ca61d19bb49b
Luke Murray <wubulous@lukemurray.co.uk>
2011-06-23 11:22:43 +0100

    Conformed to style requirements

--- xml_parse.js
+++ xml_parse.js
@@ -4,16 +4,12 @@
  * msg contains the authentificator if it was valid
  * and an error message if it wasn't
  */
-function is_valid_auth(xml)
-{
+function is_valid_auth(xml) {
     errcode = xml.getElementsByTagName("error_num");
-    if(errcode.length == 0)
-    {
+    if(errcode.length == 0) {
         //No errors, return 0 and authenificator
         return {err_code:0, msg:xml.getElementsByTagName("authenticator")[0].childNodes[0].nodeValue}
-    }
-    else
-    {
+    } else {
         //Error occurred, return error code and error message
         return {err_code:xml.getElementsByTagName("error_num")[0].childNodes[0].nodeValue, 
                 msg:xml.getElementsByTagName("error_msg")[0].childNodes[0].nodeValue}
@@ -24,8 +20,7 @@ function is_valid_auth(xml)
  * Returns true if the server has given the client work,
  * returns false otherwise
  */
-function has_work(xml)
-{
+function has_work(xml) {
     return (xml.getElementsByTagName("file_info").length > 0)
 }
 
@@ -33,16 +28,11 @@ function has_work(xml)
  * Returns the host id if it exists
  * returns null otherwise
  */
-function get_host_id_from_scheduler_request(xml)
-{
-    if(xml.getElementsByTagName("hostid").length == 0)
-    {
+function get_host_id_from_scheduler_request(xml) {
+    if(xml.getElementsByTagName("hostid").length == 0) {
         return null;
-    }
-    else
-    {
+    } else {
         var e = xml.getElementsByTagName("hostid")[0];
         return e.childNodes[0].nodeValue;
     }
-    return 
 }
