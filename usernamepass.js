5deef6f011e9406d3651899e0b215742b3cf6e78
Sam Phippen <samphippen@googlemail.com>
2011-07-05 11:43:36 +0100

    added a comment to the getting password function
    Signed-off-by: Sam Phippen <samphippen@googlemail.com>

--- usernamepass.js
+++ usernamepass.js
@@ -1,4 +1,7 @@
 
+
+//gets the hex encoded md5 of the password and username concatenated
+//this is used to autenticate against the boinc server
 function hash_username_password(user, pass)
 {
     return hex_md5(pass+user.toLowerCase())
