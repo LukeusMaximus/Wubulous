c969c337583faf62186d951d7880421285eb4b76
Luke Murray <luke@MyBow.(none)>
2011-07-15 13:47:33 +0100

    changed work report string to include key search space offset

--- des_challenge_2.js
+++ des_challenge_2.js
@@ -12,8 +12,8 @@
         ,0x40,0xe2,0x65,0x76,0x3b,0xd6,0x6c,0x38);
         this.iv = String.fromCharCode(0x09,0xf0,0x04,0x15,0xc7,0xc3,0x6d,0x8e);
         this.count = 0;
-        this.result = "0";
         this.start = String.fromCharCode(0x00,0x11,0x22,0x33,0x44,0x55,0x00,0x00);
+        this.result = "0," + this.convertToHex(this.start);
         this.keys_to_test = 0xffff;
         this.key = "";
     },
@@ -27,7 +27,7 @@
             result = result.substring(0, result.length - 16);
             //fast fail on non-printable characters
             if(this.is_printable(result)) {
-                this.result = "1," + this.convertToHex(current_key) + "," + result;
+                this.result = "1," + this.convertToHex(this.start) + ',' + this.convertToHex(current_key) +  ',' + result;
             }
             this.count++;
         }
@@ -68,7 +68,7 @@
     },
 
     "is_done":function() {
-        if (this.count >= this.keys_to_test || this.result != "0") {
+        if (this.count >= this.keys_to_test || this.result.charAt(0) == '1') {
             safe_log(this.convertToHex(this.key));
             return true;
         }
