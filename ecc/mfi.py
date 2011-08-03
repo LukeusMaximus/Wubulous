c9407bd0806bcec5b968b78920b88132fbde435e
= <=>
2011-08-03 13:01:21 +0100

    made point generation lol fast

--- ecc/mfi.py
+++ ecc/mfi.py
@@ -7,6 +7,7 @@ class mfi:
         else:
             self.val = gmpy.mpz(i)
         self.mod = gmpy.mpz(n)
+        self.val = self.val % self.mod
     #comparisons
     def __eq__(self, x):
         if str(x.__class__).find("int") != -1:
@@ -47,14 +48,49 @@ class mfi:
         if str(x.__class__).find("int") != -1:
             x = mfi(x, self.mod)
         return mfi((self.val * x.val) % self.mod, self.mod)
+    def __pow__(self, x, mod=None):
+        if mod == None:
+            mod = self.mod
+        
+        return mfi(self.val ** x, mod)
     def modinv(self):
         return mfi(gmpy.invert(self.val, self.mod), self.mod)
+    def is_square(self):
+        return self.val.is_square()
+    def sqrt(self):
+        return mfi(gmpy.sqrt(self.val), self.mod)
+    def legendre(self):
+        return mfi(self.__pow__((self.mod - 1) / 2), self.mod)
+    def jacobi(self):
+        a = self.val
+        b = self.mod
+        if b <= 0 or b & 1 == 0:
+            return mfi(0, self.mod)
+        j = 1
+        if a < 0:
+            a = -a
+            if b % 4 == 3:
+                j = -j
+        while a != 0:
+            while a & 1 == 0:
+                a = a >> 1
+                if b % 8 == 3 or b % 8 == 5:
+                    j = -j
+            c = a
+            a = b
+            b = c
+            if a % 4 == 3 and b % 4 == 3:
+                j = -j
+            a = a % b
+        if b == 1:
+            return mfi(j, self.mod)
+        else:
+            return mfi(0, self.mod)
     #converters
     def __int__(self):
         return int(self.val)
     def __str__(self):
         return str(self.val)
-
     def __repr__(self):
         return self.__str__()
 
@@ -77,6 +113,10 @@ def main():
     print str(c == a) #True
     print str(-b) #2
     print str(-a) #3
+    print str(a ** 4) #4
+    d = mfi(12345, 331)
+    print d.legendre() #-1
+    print d.jacobi() #-1
 
 if __name__ == "__main__":
     main()
